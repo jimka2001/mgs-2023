32fdee01fe981de4c70132594d370611756b8866
Jim Newton <jimka.issy@gmail.com>
2023-05-08 08:42:43 +0000

    edited function to take two arguments

---
+++ src/hello/hello_jim.py
@@ -0,0 +1,10 @@
+# this file was copied from hello.py
+
+def hello(name1, name2):
+    print("hello, " + name1)
+    print("hello, " + name2)
+
+# now we call the function, hello, with an argument
+hello("jim", "newton")
+hello("jim", "smith")
+hello("fred", "smith")
