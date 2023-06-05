24a2d00b723ea2d0a796900d47a8759934be3f45
npc48c <nolancanoy@gmail.com>
2023-06-05 16:14:42 +0000

    named under Nolan

---
+++ src/hello/nolan.py
@@ -0,0 +1,11 @@
+# define a function which takes a single argument
+#def hello(name):
+   # print("hello " + name)
+
+# call the function with an argument
+#hello("gertrude")
+
+def sequence(nu):
+    print(f"the sequence is, {nu-1} {nu} {nu+1}")
+
+sequence("4")
