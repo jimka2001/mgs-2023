774b943434901d7e4a75f73611db2660dc3639d4
LayZ Caffeine <kadenceanding@gmail.com>
2023-06-07 16:31:33 +0000

    finished

--- src/hello/test.py
+++ src/hello/test.py
@@ -1,11 +1,9 @@
-#send help pls
-
 def gold_rat(x,n):
     if n == 0:
         return(x)
     else:
-        return(1/(1 + gold_rat(x, n-1)))
-
+        return(1/(1 + gold_rat(x,n-1)))
+        
 print(gold_rat(int(input("""
 x value here
 >>>""")), int(input("""
