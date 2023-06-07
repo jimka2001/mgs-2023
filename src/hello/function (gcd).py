59f8825376e9f58aff24016726b7eae6936f4dc1
npc48c Canoy <nolancanoy@gmail.com>
2023-06-07 16:27:51 +0000

    added division count

--- src/hello/function (gcd).py
+++ src/hello/function (gcd).py
@@ -1,14 +1,15 @@
-
+count=1
 def gcd(a,b):
+    global count
     assert a>b, "First number has to be larger than the second number"
     assert b>0, "Neither number can be zero"
     if(a%b) == 0:
         return b
-    else:
+    while (a%b)!=0:
+        count+=1
         return (gcd(b, a % b))
-a=43830
-b=690
+a=462846284
+b=37284560
 c= gcd(a,b)
 print (f" The GCD of {a} and {b} is {c}.")
-
-
+print(f" The number of divisions in the system was {count}.")
