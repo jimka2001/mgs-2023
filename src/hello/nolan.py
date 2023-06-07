c1c12594382540e2b60ca37137e06b18d873ffea
npc48c Canoy <nolancanoy@gmail.com>
2023-06-07 14:01:58 +0000

    just added a Lines

--- src/hello/nolan.py
+++ src/hello/nolan.py
@@ -1,11 +1,15 @@
-# define a function which takes a single argument
+#define a function which takes a single argument
 #def hello(name):
-   # print("hello " + name)
+    #print("hello " + name)
 
-# call the function with an argument
+#call the function with an argument
 #hello("gertrude")
 
-def sequence(nu):
-    print(f"the sequence is, {nu-1} {nu} {nu+1}")
+def factorial(n):
+    assert n>=0, f"cannot compute factorial of {n} "
+    if n==0:
+        return 1
+    else:
+        return n * factorial(n-1)
 
-sequence("4")
+print(factorial(5))
