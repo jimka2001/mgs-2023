4603488b7d4b1af937baa7347797d234bc4c92ec
LayZ Caffeine <kadenceanding@gmail.com>
2023-06-12 16:24:19 +0000

    no comment

--- src/looping/linearDiophantineEquations.py
+++ src/looping/linearDiophantineEquations.py
@@ -1,11 +1,15 @@
 import math
-def LDE(a,b,c,n):
-    GCD = math.gcd(a,b)
+
+
+def LDE(a, b, c, n):
+    GCD = math.gcd(a, b)
     if c % GCD == 0:
-        return(any(print((x,y))
-        for x in range(n+1)
-        for y in range(n+1)
-        if a * x + b * y == c))
+        return [(x, y)
+                    for x in range(n+1)
+                    for y in range(n+1)
+                    if a * x + b * y == c]
     else:
         print('No solution')
-LDE(10,15,55,int(input('''>>>''')))
+
+
+print(LDE(2, 4, 28, int(input('''>>>'''))))
