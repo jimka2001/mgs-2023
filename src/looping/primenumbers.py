0b80cc627e578a617fd568027eadcdab71c95548
onsightbykanyewest ON SIGHT <leviastevens@gmail.com>
2023-06-12 14:31:15 +0000

    edited input statements

--- src/looping/primenumbers.py
+++ src/looping/primenumbers.py
@@ -1,10 +1,10 @@
-n = int(input("enter the smallest value"))
-m = int(input("enter the largest value"))
-print("all the prime numbers between", n ,"and", m ,"are:")
-for num in range(n,m):
+n = int(input("enter the smallest value "))
+m = int(input("enter the largest value "))
+print("all the prime numbers between", n, "and", m, "are:")
+for num in range(n, m):
     if num > 1:
-        for i in range(2,num):
-            if(num % i) == 0:
+        for i in range(2, num):
+            if (num % i) == 0:
                 break
         else:
             print(num, end=" ")
