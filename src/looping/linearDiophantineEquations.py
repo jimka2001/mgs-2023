c14fc88746d53fcabd5396115d81480acc12aed7
LayZ Caffeine <kadenceanding@gmail.com>
2023-06-12 15:57:46 +0000

    now finished

--- src/looping/linearDiophantineEquations.py
+++ src/looping/linearDiophantineEquations.py
@@ -3,8 +3,8 @@ def LDE(a,b,c,n):
     GCD = math.gcd(a,b)
     if c % GCD == 0:
         return(any(print((x,y))
-        for x in range(n)
-        for y in range(n)
+        for x in range(n+1)
+        for y in range(n+1)
         if a * x + b *y == c))
     else:
         print('No solution')
