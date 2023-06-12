3eb8f1f3f1ed5331a371d8cda4ee4a726a706ee5
onsightbykanyewest ON SIGHT <leviastevens@gmail.com>
2023-06-12 15:27:08 +0000

    bugged modified code

--- src/looping/primenumbersfast.py
+++ src/looping/primenumbersfast.py
@@ -1,11 +1,13 @@
-
+import math
 
 n = int(input("enter the smallest value "))
 m = int(input("enter the largest value "))
+q = math.sqrt(m)
+b = math.floor(q)
 print("all the prime numbers between", n, "and", m, "are:")
 for num in range(n, m):
     if num > 1:
-        for i in range(2, num):
+        for i in range(2, (b-3)):
             if (num % i) == 0:
                 break
         else:
